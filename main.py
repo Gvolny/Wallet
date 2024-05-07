3fdbf276d65f3fe1e41d9dd1c1d9dc5d6b279b18
gvolny <ilononemask@gmail.com>
2024-05-07 12:13:54 +0300

    oop

--- main.py
+++ main.py
@@ -1,91 +1,93 @@
 import argparse
 import os
 import sys
-import uuid
 
+class FinancialRecord:
+    def __init__(self, record_id, date, category, amount, description):
+        self.record_id = record_id
+        self.date = date
+        self.category = category
+        self.amount = amount
+        self.description = description
 
-def read_records(filename):
-    records = []
-    if os.path.exists(filename):
-        with open(filename, "r") as file:
-            record = {}
-            for line in file:
-                line = line.strip()
-                if not line:
-                    records.append(record)
-                    record = {}
-                else:
-                    key, value = line.split(": ")
-                    record[key] = value
-    return records
+class FinancialManager:
+    def __init__(self, filename):
+        self.filename = filename
+        self.records = self.read_records()
 
+    def read_records(self):
+        records = []
+        if os.path.exists(self.filename):
+            with open(self.filename, "r") as file:
+                record = {}
+                for line in file:
+                    line = line.strip()
+                    if not line:
+                        records.append(record)
+                        record = {}
+                    else:
+                        key, value = line.split(": ")
+                        record[key] = value
+        return records
 
-def write_records(filename, records):
-    with open(filename, "w") as file:
-        for record in records:
-            for key, value in record.items():
-                file.write(f"{key}: {value}\n")
-            file.write("\n")
+    def write_records(self):
+        with open(self.filename, "w") as file:
+            for record in self.records:
+                for key, value in record.items():
+                    file.write(f"{key}: {value}\n")
+                file.write("\n")
 
+    def edit_record(self, record_id, new_date, new_amount, new_description):
+        for record in self.records:
+            if record["ID"] == record_id:
+                record["Дата"] = new_date
+                record["Сумма"] = new_amount
+                record["Описание"] = new_description
+                break
 
-def edit_record(records, record_id, new_date, new_amount, new_description):
-    for record in records:
-        if record["ID"] == record_id:
-            record["Дата"] = new_date
-            record["Сумма"] = new_amount
-            record["Описание"] = new_description
-            break
+    def display_balance(self):
+        total_income = sum(
+            float(record["Сумма"]) for record in self.records if record["Категория"] == "Доход"
+        )
+        total_expense = sum(
+            float(record["Сумма"]) for record in self.records if record["Категория"] == "Расход"
+        )
+        balance = total_income - total_expense
+        print(f"Текущий баланс: {balance}")
+        print(f"Доходы: {total_income}")
+        print(f"Расходы: {total_expense}")
 
+    def add_expense(self, date, amount, description, last_id):
+        new_record = {
+            "ID": str(last_id + 1),
+            "Дата": date,
+            "Категория": "Расход",
+            "Сумма": amount,
+            "Описание": description,
+        }
+        self.records.append(new_record)
 
-def display_balance(records):
-    total_income = sum(
-        float(record["Сумма"]) for record in records if record["Категория"] == "Доход"
-    )
-    total_expense = sum(
-        float(record["Сумма"]) for record in records if record["Категория"] == "Расход"
-    )
-    balance = total_income - total_expense
-    print(f"Текущий баланс: {balance}")
-    print(f"Доходы: {total_income}")
-    print(f"Расходы: {total_expense}")
+    def add_income(self, date, amount, description, last_id):
+        new_record = {
+            "ID": str(last_id + 1),
+            "Дата": date,
+            "Категория": "Доход",
+            "Сумма": amount,
+            "Описание": description,
+        }
+        self.records.append(new_record)
 
+    def search_by_category(self, category):
+        return [record for record in self.records if record["Категория"] == category]
 
-def add_expense(records, date, amount, description, last_id):
-    new_record = {
-        "ID": str(last_id + 1),
-        "Дата": date,
-        "Категория": "Расход",
-        "Сумма": amount,
-        "Описание": description,
-    }
-    records.append(new_record)
+    def search_by_description(self, description):
+        return [record for record in self.records if description in record["Описание"]]
 
+    def search_by_date(self, date):
+        return [record for record in self.records if record["Дата"] == date]
 
-def add_income(records, date, amount, description, last_id):
-    new_record = {
-        "ID": str(last_id + 1),
-        "Дата": date,
-        "Категория": "Доход",
-        "Сумма": amount,
-        "Описание": description,
-    }
-    records.append(new_record)
-
-
-def search_by_category(records, category):
-    return [record for record in records if record["Категория"] == category]
-
-
-def search_by_description(records, description):
-    return [record for record in records if description in record["Описание"]]
-
-
-def search_by_date(records, date):
-    return [record for record in records if record["Дата"] == date]
-
-
-def search_by_amount(records, amount):
-    return [record for record in records if float(record["Сумма"]) == amount]
+    def search_by_amount(self, amount):
+        return [record for record in self.records if float(record["Сумма"]) == amount]
 
 
 def parse_args():
@@ -137,44 +139,43 @@ def parse_args():
     return parser.parse_args()
 
 
-def execute_command(args, records, last_id):
+def execute_command(args, financial_manager, last_id):
     if args.show:
-        display_balance(records)
+        financial_manager.display_balance()
     elif args.add_expense:
         date, amount, description = args.add_expense
-        add_expense(records, date, amount, description, last_id)
+        financial_manager.add_expense(date, amount, description, last_id)
     elif args.add_income:
         date, amount, description = args.add_income
-        add_income(records, date, amount, description, last_id)
+        financial_manager.add_income(date, amount, description, last_id)
     elif args.search_category:
-        category_results = search_by_category(records, args.search_category)
+        category_results = financial_manager.search_by_category(args.search_category)
         for result in category_results:
             print(result)
     elif args.search_date:
-        date_results = search_by_date(records, args.search_date)
+        date_results = financial_manager.search_by_date(args.search_date)
         for result in date_results:
             print(result)
     elif args.search_amount:
-        amount_results = search_by_amount(records, float(args.search_amount))
+        amount_results = financial_manager.search_by_amount(float(args.search_amount))
         for result in amount_results:
             print(result)
     elif args.search_description:
-        description_results = search_by_description(records, args.search_description)
+        description_results = financial_manager.search_by_description(args.search_description)
         for result in description_results:
             print(result)
     elif args.edit_record:
         record_id, new_date, new_amount, new_description = args.edit_record
-        edit_record(records, record_id, new_date, new_amount, new_description)
+        financial_manager.edit_record(record_id, new_date, new_amount, new_description)
 
 
 def main():
     filename = "finances.txt"
-    records = read_records(filename)
+    financial_manager = FinancialManager(filename)
     args = parse_args()
-    last_id = max(int(record["ID"]) for record in records) if records else 0
-    execute_command(args, records, last_id)
-    write_records(filename, records)
-
+    last_id = max(int(record["ID"]) for record in financial_manager.records) if financial_manager.records else 0
+    execute_command(args, financial_manager, last_id)
+    financial_manager.write_records()
 
 if __name__ == "__main__":
     main()
